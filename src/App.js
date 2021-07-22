58a3a7d7fbd45cad5e286c35a6a88568236f6d2d
henriquecesp <henriquecesps4@gmail.com>
2021-07-21 22:38:27 -0300

    ✨ initialize react-app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import User from "./components/User";
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Login />
+        <br />
+        <User />
+        <br />
+        <Logout />
       </header>
     </div>
   );
